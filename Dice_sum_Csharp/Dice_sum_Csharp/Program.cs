2e02990e629a2ab5de27860413aec9fc71893d7a
ddnb <damian.bevan@aceaxis.co.uk>
2020-02-14 17:14:46 +0000

    First build of Csharp solution

---
+++ Dice_sum_Csharp/Dice_sum_Csharp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dice_sum_Csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Entry stuff
+            Console.Title = "Dice sum";
+
+
+            // Exit stuff
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+        }
+    }
+}
